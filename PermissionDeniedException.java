251e78f5e5a801f9e9e6df945ff1e233b9767a67
SirAfino <47832620+SirAfino@users.noreply.github.com>
2020-01-13 12:20:37 +0100

    Delete PermissionDeniedException.java

--- PermissionDeniedException.java
+++
@@ -1,9 +0,0 @@
-public class PermissionDeniedException extends Exception
-{
-	private static final long serialVersionUID = 1L;
-
-	public PermissionDeniedException(String errorMessage)
-	{
-		super(errorMessage);
-	}
-}
