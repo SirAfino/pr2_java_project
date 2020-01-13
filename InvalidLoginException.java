edf72b1e182ef1373d9da83fb4f44e7101a10514
SirAfino <47832620+SirAfino@users.noreply.github.com>
2020-01-13 12:16:56 +0100

    Delete InvalidLoginException.java

--- InvalidLoginException.java
+++
@@ -1,9 +0,0 @@
-public class InvalidLoginException extends Exception
-{
-	private static final long serialVersionUID = 1L;
-
-	public InvalidLoginException(String errorMessage)
-	{
-		super(errorMessage);
-	}
-}
